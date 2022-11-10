173d435eab7818f2252194966defe361e22d0139
Threde <anphmvn75@gmail.com>
2022-11-10 23:22:01 +0700

    updated

--- testt.cpp
+++ testt.cpp
@@ -1,11 +1,40 @@
 #include <iostream>
 #include <string>
+#include <iomanip>
 
 using namespace std;
 
-int main(){
-    string a;
-    getline(cin, a);
-    cout << a;
+void Nhap(string&, float&, float&);
+void Xuly(float&, float, float&);
+void Xuat(string, float, float, float);
+int main()
+{
+    string Hoten;
+    float Diemtoan;
+    float Diemvan;
+    float DTB;
+    void Nhap(string& Hoten, float& Diemtoan, float& Diemvan);
+    float Xuly(float Diemtoan, float Diemvan, float& DTB);
+    void Xuat(string Hoten, float Diemtoan, float Diemvan, float DTB);
     return 0;
+}
+void Nhap(string& Hoten, float& Diemtoan, float& Diemvan)
+{
+    cout << "nhap ho ten : " << endl;
+    getline(cin, Hoten);
+    cout << " Nhap diem toan: " << endl;
+    cin >> Diemtoan;
+    cout << " Nhap diem van: " << endl;
+    cin >> Diemvan;
+}
+float Xuly(float Diemtoan, float Diemvan, float& DTB)
+{
+    return DTB = float((Diemtoan + Diemvan) / 2);
+}
+void xuat(string Hoten, float Diemtoan, float Diemvan, float DTB)
+{
+    cout << " Ho ten la" << endl << Hoten;
+    cout << "diem toan:" << endl << Diemtoan;
+    cout << "diem van:" << endl << Diemvan;
+    cout << " diem trung binh la: " << endl << DTB;
 }
