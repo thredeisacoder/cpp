f980eaa5c68cb4303de5d778dfac90bc78d92bee
Threde <anphmvn75@gmail.com>
2022-12-06 18:38:42 +0700

    Add Files

--- Exams/bai4.cpp
+++ Exams/bai4.cpp
@@ -5,19 +5,28 @@ using namespace std;
 int main(){
     int n = 12;
     // cin >> n;
-    int array[100]= {-4, 5, 0, 6, -1, 0, 8, 3, -11, 15, 2, 1};
+    int array[n]= {-4, 5, 0, 6, -1, 0, 8, 3, -11, 15, 2, 1};
     // for(int i=0; i<n; i++){
     //     cin >> array[i];
     // }
     for (int i = 0; i < n; i++){
         for(int j = i + 1; j < n; j++){
-            if(array[i] < array[j]){
+            if(array[i] > array[j]){
                 int temp = array[i];
                     array[i] = array[j];
                     array[j] = temp;
             }
         }
-        cout << array[i] << " ";
+        for(int h = i + 1; h < n; h++){
+            if(array[i] > 0){
+                if(array[i] < array[h]){
+                    int temp = array[i];
+                        array[i] = array[h];
+                        array[h] = temp;
+                }
+            }
+        }
+        cout << array[i] << " "; //em da co gang het suc huhu
     }
     return 0;
 }
--- Exams/tempCodeRunnerFile.cpp
+++
@@ -1,9 +0,0 @@
-or(int h = i + 1; h < n; h++){
-        //     if(array[i] < 0){
-        //         while(array[i] < array[h]){
-        //             int temp2 = array[i];
-        //                 array[i] = array[h];
-        //                 array[h] = temp2;
-        //         }
-        //     }
-        // }
